6a9e0e7ea56f5bb246f674039d91b42d434a73a6
Jason Holtkamp <holtkam2@gmail.com>
2023-04-02 10:23:01 -0700

    Refactored to use S3LoaderImports

--- langchain/src/document_loaders/s3.ts
+++ langchain/src/document_loaders/s3.ts
@@ -3,68 +3,71 @@ import path from 'path';
 import os from 'os';
 import { BaseDocumentLoader } from "./base.js";
 import { UnstructuredLoader } from './unstructured.js';
-// import { UnstructuredLoader } from "./unstructured.js";
 
 export class S3Loader extends BaseDocumentLoader {
-  bucket: string;
+  private bucket: string;
 
-  key: string;
+  private key: string;
 
-  unstructuredAPIUrl: string;
+  private unstructuredAPIURL: string;
 
-  constructor(bucket: string, key: string, unstructuredAPIUrl: string) {
+  constructor(bucket: string, key: string, unstructuredAPIURL: string) {
     super();
-    this.unstructuredAPIUrl = unstructuredAPIUrl;
     this.bucket = bucket;
     this.key = key;
+    this.unstructuredAPIURL = unstructuredAPIURL;
   }
 
-  async load() {
-    try {
-      const { S3Client, GetObjectCommand } = await import('@aws-sdk/client-s3' as any);
+  public async load() {
+    const { S3Client, GetObjectCommand } = await S3LoaderImports();
 
-      const s3Client = new S3Client();
+    const s3Client = new S3Client();
 
-      const getObjectCommand = new GetObjectCommand({
-        Bucket: this.bucket,
-        Key: this.key,
-      });
+    const getObjectCommand = new GetObjectCommand({
+      Bucket: this.bucket,
+      Key: this.key,
+    });
 
-      const response = await s3Client.send(getObjectCommand);
+    const response = await s3Client.send(getObjectCommand);
 
-      const objectData = await new Promise<Buffer>((resolve, reject) => {
-        const chunks: Buffer[] = [];
-        response.Body.on("data", (chunk: any) => chunks.push(chunk));
-        response.Body.on("end", () => resolve(Buffer.concat(chunks)));
-        response.Body.on("error", reject);
-      });
+    const objectData = await new Promise<Buffer>((resolve, reject) => {
+      const chunks: Buffer[] = [];
+      response.Body.on("data", (chunk: any) => chunks.push(chunk));
+      response.Body.on("end", () => resolve(Buffer.concat(chunks)));
+      response.Body.on("error", reject);
+    });
 
-      const tempDir = fs.mkdtempSync(path.join(os.tmpdir(), 's3fileloader-'));
+    const tempDir = fs.mkdtempSync(path.join(os.tmpdir(), 's3fileloader-'));
 
-      const filePath = path.join(tempDir, this.key);
+    const filePath = path.join(tempDir, this.key);
 
-      fs.mkdirSync(path.dirname(filePath), { recursive: true });
+    fs.mkdirSync(path.dirname(filePath), { recursive: true });
 
-      fs.writeFileSync(
-        filePath, 
-        objectData
-      );
+    fs.writeFileSync(
+      filePath, 
+      objectData
+    );
 
-      console.log(`Downloaded file ${this.key} from S3 bucket ${this.bucket} to ${filePath}`);
+    console.log(`Downloaded file ${this.key} from S3 bucket ${this.bucket} to ${filePath}`);
 
-      const unstructuredLoader = new UnstructuredLoader(
-        this.unstructuredAPIUrl,
-        filePath
-      );
+    const unstructuredLoader = new UnstructuredLoader(
+      this.unstructuredAPIURL,
+      filePath
+    );
 
-      const docs = await unstructuredLoader.load();
+    const docs = await unstructuredLoader.load();
 
-      return docs;
-    } catch (e) {
-      console.error(e);
-      throw new Error(
-        "Failed to load aws-sdk'. Please install it with eg. `npm install aws-sdk'`."
-      );
-    }
+    return docs;
+  }
+}
+
+async function S3LoaderImports() {
+  try {
+    return await import('@aws-sdk/client-s3' as any);
+  } catch (e) {
+    console.error(e);
+    throw new Error(
+      "Failed to load @aws-sdk/client-s3'. Please install it eg. `yarn add @aws-sdk/client-s3`."
+    );
   }
 }
