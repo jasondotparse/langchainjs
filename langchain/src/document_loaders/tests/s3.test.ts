ef2a73ee97a92b674db20312947dc02cae653011
Jason Holtkamp <holtkam2@gmail.com>
2023-04-03 09:07:02 -0700

    Mocked the s3 client in the unit test

--- langchain/src/document_loaders/tests/s3.test.ts
+++ langchain/src/document_loaders/tests/s3.test.ts
@@ -1,7 +1,32 @@
-import { test } from '@jest/globals';
+import { test, jest } from '@jest/globals';
+import { S3Loader } from '../s3.js';
 
-test('S3Loader loads a document from S3 and processes it with the UnstructuredLoader', async () => {
+jest.mock('@aws-sdk/client-s3', () => ({
+    S3Client: jest.fn().mockImplementation(() => ({
+      send: jest.fn().mockImplementation(() => ({
+          Body: {
+            on: jest.fn().mockImplementation((event: any, callback: any) => {
+              if (event === 'data') {
+                console.log('Mocking S3 file download... chunk received');
+                callback(Buffer.from('Mock file content'));
+              } else if (event === 'end') {
+                console.log('Mocking S3 file download... end');
+                callback(undefined);
+              }
+            }),
+            pipe: jest.fn(),
+          }
+        }))
+    })),
+    GetObjectCommand: jest.fn(),
+  })
+);
 
-  expect(1).toEqual(1);
-
+test('Test S3 loader', async () => {
+  const loader = new S3Loader(
+    "test-bucket-123", 
+    "AccountingOverview.pdf", 
+    "http://localhost:8000/general/v0/general"
+  );
+  await loader.load();
 });
